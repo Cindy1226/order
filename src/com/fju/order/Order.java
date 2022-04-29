1fe710687357d4d1b1bd9bd3860a85c02ddfd649
cindy1226 <cindywawa91@gmail.com>
2022-04-29 16:02:52 +0800

    1

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -1,11 +1,24 @@
 package com.fju.order;
 
 public class Order {
+    int shipFeed;
     int amount;
     Delivery delivery;
+
+
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed= delivery.price;
     }
+
+    public int total(){
+        return amount+shipFeed;
+
+    }
+
+
+
+
 }
